7f52089dedc3792c1d7a9b8d405dfa3658e2f3a7
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-01 23:19:23 +0530

    Primitive Datatype

---
+++ Variable and data type/Primitive_Datatype.java
@@ -0,0 +1,20 @@
+public class Primitive_Datatype {
+    public static void main(String args[]) {
+        byte a = 5;
+        short b = 10;
+        int c = 15; // most used
+        //long d = 20;L; // less used
+        float e = 5.5f; // less used  
+        //double f = 10.99; // most used
+        char g = 'A'; // most used
+        boolean h = true; // most used
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        //System.out.println(d);
+        System.out.println(e);
+        //System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
