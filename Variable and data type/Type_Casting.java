3b2aea04f680f7e8e6a3283c4786e64fd15e4c0f
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-15 18:56:19 +0530

    Tyep Casting

---
+++ Variable and data type/Type_Casting.java
@@ -0,0 +1,7 @@
+public class Type_Casting {
+    public static void main(String args[]) {
+        double a = 10.5;
+        int b = (int) a; // explicit type casting
+        System.out.println(b);
+    }
+}
