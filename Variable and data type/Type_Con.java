d943bc501788d00cd556d76d309cf2598bd1d770
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-15 18:54:32 +0530

    Type_Con.java

---
+++ Variable and data type/Type_Con.java
@@ -0,0 +1,10 @@
+
+
+public class Type_Con {
+    public static void main(String args[]) {
+        int a = 10;
+        double b = a; // implicit type conversion
+        System.out.println(b);
+    }
+    
+}
