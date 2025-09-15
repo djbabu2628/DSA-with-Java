34ee8a7de2a0dd34f033dedaf26d6d40632e46b8
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-15 18:10:51 +0530

    Input.java

---
+++ Variable and data type/Input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Input{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in), vc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
+
+        String str = vc.nextLine();
+        System.out.println(str);
+
+        sc.close();
+    }
+}
