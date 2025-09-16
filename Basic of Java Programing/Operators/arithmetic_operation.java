5cf26907015ee7d14c1870f0c102d48602659aec
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-16 19:04:45 +0530

    Performe some arithmetic operation

---
+++ Basic of Java Programing/Operators/arithmetic_operation.java
@@ -0,0 +1,14 @@
+
+
+public class arithmetic_operation{
+    public static void main(String args[]){
+        int A = 10;
+        int B = 10;
+        System.out.println("Add = "+ (A+B));
+        System.out.println("Sub = "+ (A-B));
+        System.out.println("Mul = "+ (A*B));
+        System.out.println("Div = "+ (A/B));
+        System.out.println("Rem = "+ (A%B));
+
+    }
+}
