7357ff3b1e3bce514de9b222e133c4499f082e25
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-16 22:21:59 +0530

    relational operators

---
+++ Basic of Java Programing/Operators/relational_operators.java
@@ -0,0 +1,15 @@
+
+
+public class relational_operators {
+    public static void main(String args[]){
+        int a = 10 ;
+        int b = 20 ;
+        System.out.println("a==b\t"+(a==b));
+        System.out.println("a!=b\t"+(a!=b));
+        System.out.println("a>b\t"+(a>b));
+        System.out.println("a<b\t"+(a<=b));
+        System.out.println("a>=b\t"+(a>=b));
+        System.out.println("a<=b\t"+(a<=b));
+
+    }    
+}
