387f508ca5cf23c0a7e4266f7976d2daff8eb89c
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-16 22:44:46 +0530

    assignment operators

---
+++ Basic of Java Programing/Operators/assignment_operators.java
@@ -0,0 +1,12 @@
+
+
+
+public class assignment_operators {
+    public static void main(String args[]){
+        int b = 5;
+        System.out.println(b += b);
+        System.out.println(b -= b);
+        System.out.println(b *= b);
+        System.out.println(b /= b);
+    }    
+}
