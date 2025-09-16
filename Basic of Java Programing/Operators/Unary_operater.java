14bce0e81dd049a69486e41f307e1a3773e38eab
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-16 19:14:59 +0530

    Using Unary Operater

---
+++ Basic of Java Programing/Operators/Unary_operater.java
@@ -0,0 +1,17 @@
+
+
+
+public class Unary_operater{
+    public static void main(String args[]){
+    int a = 6;
+    int b = 7;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(--a);
+    System.out.println(a--);
+    System.out.println(a);
+    System.out.println(b--);
+    System.out.println(--b);
+
+}
+}
