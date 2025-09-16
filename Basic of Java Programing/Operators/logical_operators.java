0aaf24e0726ce0a88020f53c843e1e3f9b0e1236
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-16 22:35:37 +0530

    logical operators

---
+++ Basic of Java Programing/Operators/logical_operators.java
@@ -0,0 +1,11 @@
+
+
+public class logical_operators {
+    public static void main(String args[]){
+        int a =10;
+        int b = 20;
+        System.out.println(a>3 && a==10);
+        System.out.println(b<4 || a==b);
+        System.out.println(!(a<=b));
+    }    
+}
