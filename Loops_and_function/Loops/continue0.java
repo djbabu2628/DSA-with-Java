28c2a137d684d2ffc0bd6790e85224b452a27b87
My-Hospital <viveksunilchavhan@gmail.com>
2025-09-27 12:04:32 +0530

    continue keyword and question on that

---
+++ Loops_and_function/Loops/continue0.java
@@ -0,0 +1,9 @@
+public class continue0 {
+    public static void main(String args[]){
+       for(int i=0;i<=6;i++){
+            if(i==3){
+                continue;
+            }System.out.println(i);
+       } 
+    }
+}
