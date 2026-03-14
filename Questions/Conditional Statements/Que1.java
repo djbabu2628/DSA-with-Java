6f5e1f4bf9994b0de9565504aeaf43613b5a4722
My-Hospital <viveksunilchavhan@gmail.com>
2026-03-15 00:55:38 +0530

    Solve 4 programing

---
+++ Questions/Conditional Statements/Que1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Que1{
+	public static void main(String args[]){
+		System.out.println("Enter num:- ");
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if(x>0){
+			System.out.println("Num is Positive " + x );
+		}
+		else{
+			System.out.println("Num is Negative " + x );
+		}
+		
+	}
+}
---
+++ Questions/Conditional Statements/You_have_fever_or_not.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Que2{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your Temp :- ");
+		double temp = sc.nextDouble();
+		if(temp>=100){
+				System.out.println("You have Fever");
+			}
+		else{
+				System.out.println("You don't have a Fever");
+			}
+		}
+	}
