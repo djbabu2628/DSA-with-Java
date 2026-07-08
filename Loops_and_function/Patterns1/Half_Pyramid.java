2fbfa3bfb350805642433eededc655f1125b5a90
My-Hospital <viveksunilchavhan@gmail.com>
2026-07-08 07:59:23 +0530

    Half Pyramid

---
+++ Loops_and_function/Patterns1/Half_Pyramid.java
@@ -0,0 +1,11 @@
+public class Half_Pyramid{
+    public static void main(String args[]){
+        for(int line=0;line<=4;line++){
+            for(int num=1;num<=line;num++){
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+
+}
