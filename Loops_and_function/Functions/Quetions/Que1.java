62ee4f814c2f3105e6336a0817a45ff21e8e6ba7
My-Hospital <viveksunilchavhan@gmail.com>
2026-07-20 22:44:49 +0530

    1st question completed

---
+++ Loops_and_function/Functions/Quetions/Que1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Que1{
+    public static int avg(int a, int b, int c){
+        int avg = (a+b+c)/3;
+        return avg;
+    }
+
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Your 1st no. ");
+    int a = sc.nextInt();
+    System.out.print("Enter Your 2nd no. ");
+    int b = sc.nextInt();
+    System.out.print("Enter Your 3rd no. ");
+    int c = sc.nextInt();
+    int avg=avg(a,b,c);
+    System.out.println("Average of Your No. is " + avg);
+    sc.close();
+}
+}
