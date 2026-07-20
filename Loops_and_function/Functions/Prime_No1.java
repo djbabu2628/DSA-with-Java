8dcda2adea0ea377b6edc0409d7c950e32ad78bd
My-Hospital <viveksunilchavhan@gmail.com>
2026-07-20 09:13:33 +0530

    Code for is no. Prime or not

---
+++ Loops_and_function/Functions/Prime_No1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Prime_No1{
+
+    public static boolean isPrime(int n){
+        boolean isPrime = true;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%2==0){
+                isPrime=false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number ");
+        int n = sc.nextInt();
+        if(n<=1){
+            System.out.print("less than 1 is not prime no. please enter greater than 1");
+        }
+        else{
+            System.out.print("is Prime " + isPrime(n));
+        }
+    }
+
+}
