d30fc1a884f549a14c10bb13b065816675754707
My-Hospital <viveksunilchavhan@gmail.com>
2026-07-21 00:03:15 +0530

    Program completed On  Decimal to Binary

--- Loops_and_function/Functions/decTobin.java
+++ Loops_and_function/Functions/decTobin.java
@@ -1,13 +1,27 @@
 import java.util.*;
-
+public class decTobin{
+    
 public static void decTobin(int n){
     int bin = 0;
     int pow = 0;
     int decNum = n;
 
     while(n>0){
-        
+        int rem = n % 2;
+        bin = bin + (rem * (int)(Math.pow(10,pow)));
+        pow++;
+        n = n / 2;
+    }
+    System.out.print("Binary of " + decNum + " is : " + bin);
     }
 
+public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Decimal no : ");
+        int n = sc.nextInt();
+        decTobin(n);
+        sc.close();
+
+    }
 
 }
