a3a21c35b6f243d2c8801be45d3d53e144e70c34
My-Hospital <viveksunilchavhan@gmail.com>
2026-07-21 22:36:03 +0530

    Code on Hollow Rectangle

---
+++ Loops_and_function/Patterns2/Hollow_Rect.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Hollow_Rect{
+
+    public static void Rect(int rows, int cols){
+        for(int i=1;i<= rows;i++){
+            for(int j=1;j<=cols;j++){
+                if(i == 1|| i==rows || j==1 || j==cols){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length : ");
+        int cols = sc.nextInt();
+        System.out.print("Enter width : ");
+        int rows = sc.nextInt();
+        Rect(cols,rows);
+        sc.close();
+        
+    }
+}
